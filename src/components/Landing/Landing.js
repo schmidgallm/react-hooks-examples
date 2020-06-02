e75e308eb573cb1d8acdb050558c0feda0da9838
schmidgallm <schmidgallm@outlook.com>
2020-06-01 20:41:49 -0700

    init syntax highlight package with code strings exported from utils

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,18 +2,31 @@ import React from 'react';
 import './Landing.css';
 
 const Landing = () => (
-	<div className="wrapper">
-		<div className="container jumbotron my-5 bg-success text-white">
-			<h1>Fun with React Hooks</h1>
-			<p>Working examples of useState, useEffect, and custom hooks</p>
-		</div>
-		<div className="container mt-5">
-			<div className="row">
-				<div className="col-lg-4 col-sm-12">State</div>
-				<div className="col-lg-4 col-sm-12">Effect</div>
-				<div className="col-lg-4 col-sm-12">Custom</div>
-			</div>
-		</div>
-	</div>
+  <div className='wrapper'>
+    <div className='container jumbotron my-5 bg-success text-white'>
+      <h1>Fun with React Hooks</h1>
+      <p>Working examples of useState, useEffect, and custom hooks</p>
+    </div>
+    <div className='container text-center'>
+      <div className='row'>
+        <div className='col-lg-6 col-sm-12 section'>
+          <h2>useState</h2>
+          <a href='/state' className='btn btn-primary'>
+            See Example
+          </a>
+        </div>
+        <div className='col-lg-6 col-sm-12 section'>
+          <h2>useEffect</h2>
+          <a href='/effect' className='btn btn-primary'>
+            See Example
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 export default Landing;
+
+/*
+
+*/
