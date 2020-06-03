50c65a83a96829aaf84cef8da07968e280970982
schmidgallm <schmidgallm@outlook.com>
2020-06-02 20:37:55 -0700

    landing ui change and use state example component added

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,24 +3,31 @@ import './Landing.css';
 
 const Landing = () => (
   <div className='wrapper'>
-    <div className='container jumbotron my-5 bg-success text-white'>
+    <div className='container jumbotron my-3 bg-success text-white'>
       <h1>Fun with React Hooks</h1>
       <p>Working examples of useState, useEffect, and custom hooks</p>
     </div>
-    <div className='container text-center'>
-      <div className='row'>
-        <div className='col-lg-6 col-sm-12 section'>
-          <h2>useState</h2>
-          <a href='/state' className='btn btn-primary'>
-            See Example
-          </a>
-        </div>
-        <div className='col-lg-6 col-sm-12 section'>
-          <h2>useEffect</h2>
-          <a href='/effect' className='btn btn-primary'>
-            See Example
-          </a>
-        </div>
+    <div className='container section-container text-center'>
+      <div className='section'>
+        <h2>useState</h2>
+        <p>
+          The useState hooks replaces the need to declare state inside
+          constructor and declare state as variable.
+        </p>
+        <a href='/state' className='btn btn-primary'>
+          See Example
+        </a>
+      </div>
+      <div className='section'>
+        <h2>useEffect</h2>
+        <p>
+          The Effect Hook lets you perform side effects in function components.
+          Replaces the need to call methods such as component did mount and
+          update.
+        </p>
+        <a href='/effect' className='btn btn-primary'>
+          See Example
+        </a>
       </div>
     </div>
   </div>
--- src/components/State/State.js
+++ src/components/State/State.js
@@ -1,13 +1,116 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
+import './State.css';
 
-const State = () => (
-	<div>
-		<h1>State</h1>
-		<h1>State</h1>
-		<h1>State</h1>
-		<h1>State</h1>
-		<h1>State</h1>
-		<h1>State</h1>
-	</div>
-);
+const State = () => {
+  // init form state hook
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+  });
+
+  // init message state hook
+  const [message, setMessage] = useState({
+    msg: '',
+  });
+
+  //   Notice above you can declare multiple state values using hooks
+
+  // destructure state into easier managable variables
+  const { first_name, last_name, email } = formData;
+  const { msg } = message;
+
+  // on change handler
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  // on submit handler
+  const onSubmit = async (e) => {
+    // prevent form from submitting
+    e.preventDefault();
+
+    // submit form and perform api call or business logic here
+    console.log(formData);
+
+    // set message
+    setMessage({
+      msg: `Hi ${first_name}! Form succesfully submitted using hooks...`,
+    });
+
+    // clear all state values back to original values
+    clearState();
+  };
+
+  // set message and timeout message and clear form
+  const clearState = () => {
+    setTimeout(() => {
+      setMessage({ msg: '' });
+      setFormData({
+        first_name: '',
+        last_name: '',
+        email: '',
+      });
+    }, 3000);
+  };
+
+  return (
+    <Fragment>
+      <div className='container my-5 form'>
+        {msg ? <h3 className='text-primary'>{msg}</h3> : null}
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor='first_name'>First Name</label>
+            <input
+              type='text'
+              className='form-control'
+              id='first_name'
+              aria-describedby='name'
+              name='first_name'
+              value={first_name}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='last_name'>Last Name</label>
+            <input
+              type='text'
+              className='form-control'
+              id='last_name'
+              aria-describedby='name'
+              name='last_name'
+              value={last_name}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='exampleInputEmail1'>Email address</label>
+            <input
+              type='email'
+              className='form-control'
+              id='exampleInputEmail1'
+              aria-describedby='emailHelp'
+              name='email'
+              value={email}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <button type='submit' className='btn btn-warning'>
+            Submit
+          </button>
+        </form>
+        <h4>
+          See the code{' '}
+          <a
+            href='https://github.com/schmidgallm/react-hooks-examples'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Here
+          </a>
+        </h4>
+      </div>
+    </Fragment>
+  );
+};
+
 export default State;
